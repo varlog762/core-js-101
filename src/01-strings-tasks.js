c97b06f5f91c8c765a793923d66aacd30fe7b81c
Gregory Pereverzev <greg.pereverzev@gmail.com>
2023-05-09 13:50:09 +0200

    feat: add unbracketTag & convertToUpperCase functions

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -152,8 +152,8 @@ function removeFirstOccurrences(str, value) {
  *   '<span>' => 'span'
  *   '<a>' => 'a'
  */
-function unbracketTag(/* str */) {
-  throw new Error('Not implemented');
+function unbracketTag(str) {
+  return str.slice(1, str.length - 1);
 }
 
 
@@ -167,8 +167,8 @@ function unbracketTag(/* str */) {
  *   'Thunderstruck' => 'THUNDERSTRUCK'
  *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  */
-function convertToUpperCase(/* str */) {
-  throw new Error('Not implemented');
+function convertToUpperCase(str) {
+  return str.toLocaleUpperCase();
 }
 
 /**
